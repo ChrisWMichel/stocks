275572e987bd1b9f86cb687cff0af9367fb10564
ChrisWMichel <christopherw.michel@gmail.com>
2020-08-05 15:36:53 -0500

    refactored searchbox auto-lookup

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,28 +1,35 @@
 <template>
   <div>
-    <div v-if="stockSym">
-      {{ updateStocks() }}
-      <VueTradingView
-        :options="{
-          symbol: stockSym,
-          theme: 'light',
-          interval: 'D',
-          width: 1000,
-          allow_symbol_change: true,
-        }"
-      />
-    </div>
+      <div v-if="stockSym">
+        {{updateStocks()}}
+        <vue-trading-view
+            :key="stockSym"
+            :options="{
+              symbol: stockSym,
+              theme: 'light',
+              interval: 'D',
+              width: 1000,
+              allow_symbol_change: true
+            }"
+        ></vue-trading-view>
+      </div>
 
     <div>
       <analysis :financeData="financialData"></analysis>
+
+
     </div>
   </div>
 </template>
 
 <script>
+
+
+
+
 import VueTradingView from "vue-trading-view";
 import Analysis from "@/components/Analysis";
-import axios from "axios";
+import axios from 'axios'
 
 export default {
   name: "Chart",
@@ -30,43 +37,45 @@ export default {
     Analysis,
     VueTradingView,
   },
-  //props:['clearDataObj'],
-  data() {
-    return {
-      url: "",
-      stockPrice: this.$store.state.stockPrice,
-      financialData: {},
-    };
+
+  data(){
+    return{
+      url:'',
+      stockPrice:this.$store.state.stockPrice,
+      financialData:{}
+    }
+
   },
-  computed: {
-    stockSym() {
-      return this.$store.state.symbol;
-    },
+  computed:{
+    stockSym(){
+      console.log('symbol', this.$store.state.symbol)
+      return this.$store.state.symbol
+    }
   },
-  methods: {
-    async updateStocks() {
-      console.log("publicToken", this.publicToken);
-      if (!this.$store.state.clearDataObj) {
-        return;
+  methods:{
+    async updateStocks(){
+
+      if(!this.$store.state.clearDataObj){
+        return
       }
-      console.log("symbol", this.$store.state.symbol);
       try {
-        await axios
-          .get(
-            `https://cloud.iexapis.com/stable/stock/${this.$store.state.symbol}/quote?token=sk_9fea5a3a643e4dc3b771899b3a642177`
-          )
-          .then((res) => {
-            this.financialData = res.data;
-            console.log("====>>>>>>>", res.data);
-          });
-      } catch (err) {
-        console.log("error", err);
+        await axios.get(
+            //  pk_b126cb1ba8dc441aa3673f3ccb2a3ba5
+            // sk_9fea5a3a643e4dc3b771899b3a642177
+            `https://cloud.iexapis.com/stable/stock/${this.$store.state.symbol}/quote?token=pk_b126cb1ba8dc441aa3673f3ccb2a3ba5`
+        )
+            .then((res) => {
+              this.financialData = res;
+              console.log("financialData", res.data);
+              this.$store.state.stockPrice = this.financialData.data.latestPrice;
+            });
+      } catch (err){
+        console.log('error', err)
       }
 
       this.$store.state.clearDataObj = false;
-      console.log("financialData", this.financialData);
-      this.$store.state.stockPrice = this.financialData.latestPrice;
-    },
-  },
+      this.forceUpdate();
+    }
+  }
 };
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,63 +3,42 @@
     <b-row class="dropdown searchBox" style="margin-left: 12%"
       ><!--class="justify-content-md-center"-->
       <b-col cols="7" lg="7" md="5" sm="7" style="padding-right: 0">
-        <input
-          class="dropdown-input"
-          v-model.trim="filterText"
-          type="text"
-          style="width: 100%;"
-          placeholder="Search Stocks"
-        />
-        <div class="text-center">
-          <small>Click the 'Clear' button for new search</small>
-        </div>
-        <div v-show="filterText" class="dropdown-list">
-          <div
-            v-for="item in filteredNames"
-            :key="item.symbol"
-            @click.prevent="storeData(item)"
-            class="dropStocks"
-          >
-            {{ item.name || "N/A" }}: ({{ item.symbol }})
-          </div>
-        </div>
+
+        <auto-search @getStock="getStock"></auto-search>
+
+
       </b-col>
-      <b-col cols="1" style="padding: 0">
+      <!--<b-col cols="1" style="padding: 0">
         <button size="sm" class="" @click="clearData">Clear</button>
-      </b-col>
+      </b-col>-->
       <b-col cols="4" style="padding-left: 5%" class="top-label">
         {{ getSockPrice || 0 | currency }}
       </b-col>
     </b-row>
-    <chart class="chart"></chart>
+    <chart class="chart" ref="updateStock"></chart>
   </b-container>
 </template>
 
 <script>
-import { stockMixin } from "@/mixins/stockMixin";
 import Chart from "./Chart";
+import AutoSearch from "@/components/AutoSearch";
 
 export default {
-  // name: "Home",
+   name: "Home",
   components: {
+    AutoSearch,
     Chart,
   },
-  mixins: [stockMixin],
   data() {
     return {
-      stockName: "",
-      stockSymbol: "",
       showList: true,
     };
   },
   methods: {
-    storeData(item) {
-      this.$store.state.symbol = item.symbol;
-      this.stockName = item.name;
-      this.stockSymbol = item.symbol;
-      this.filterText = `${this.stockName} (${this.stockSymbol} )`;
-      this.showList = false;
-    },
+     getStock(){
+        this.$refs.updateStock.updateStocks()
+     },
+
     clearData() {
       this.filterText = "";
       this.stockName = "";
@@ -67,6 +46,7 @@ export default {
       this.$store.state.symbol = "";
       this.$store.state.stockPrice = 0;
       this.$store.state.clearDataObj = true;
+      this.$refs.typeBox.focus()
     },
   },
   computed: {
