218bac41f1e5b4d20233874451dde04199eb0479
ChrisWMichel <christopherw.michel@gmail.com>
2020-08-05 17:10:59 -0500

    Added technical breakdown

--- src/components/Analysis.vue
+++ src/components/Analysis.vue
@@ -3,23 +3,23 @@
     <!--justify-content-sm-center justify-content-md-center justify-content-lg-center-->
     <b-row>
       <b-col md="12" class="p-3">
-        <h4 >Description</h4>
+        <h4>Description</h4>
       </b-col>
     </b-row>
-    <b-row >
-        <b-col>
-          <b-form-textarea
-              style="border: none"
-            id="textarea"
-            v-model="description"
-            rows="5"
-            class="m-auto"
-          ></b-form-textarea>
-        </b-col>
+    <b-row>
+      <b-col>
+        <b-form-textarea
+          style="border: none"
+          id="textarea"
+          v-model="description"
+          rows="5"
+          class="m-auto"
+        ></b-form-textarea>
+      </b-col>
     </b-row>
     <b-row style="display: flex;" class="pt-4">
       <b-col md="1"></b-col>
-      <b-col md="4" class="pr-5" >
+      <b-col md="4" class="pr-5">
         <b-table-simple hover small caption-top responsive>
           <b-thead head-variant="dark">
             <b-tr>
@@ -29,19 +29,19 @@
           <b-tbody>
             <b-tr>
               <b-th class="text-center">TRENDING BAND:</b-th>
-              <b-th class="text-left">{{trendingBand}}</b-th>
+              <b-th class="text-left">{{ financeData }}</b-th>
             </b-tr>
             <b-tr>
               <b-th class="text-center">RSI:</b-th>
-              <b-th class="text-left">{{rsi}}</b-th>
+              <b-th class="text-left">{{ rsiData }}</b-th>
             </b-tr>
             <b-tr>
               <b-th class="text-center">MACD:</b-th>
-              <b-th class="text-left">{{macd}}</b-th>
+              <b-th class="text-left">{{ macd }}</b-th>
             </b-tr>
             <b-tr>
               <b-th class="text-center">VOLUME:</b-th>
-              <b-th class="text-left">{{volume}}</b-th>
+              <b-th class="text-left">{{ volume }}</b-th>
             </b-tr>
           </b-tbody>
         </b-table-simple>
@@ -57,19 +57,19 @@
           <b-tbody>
             <b-tr>
               <b-th class="text-center">PRICE TO EARNINGS:</b-th>
-              <b-th class="text-left">{{priceToEarnings}}</b-th>
+              <b-th class="text-left">{{ priceToEarnings }}</b-th>
             </b-tr>
             <b-tr>
               <b-th class="text-center">EBITDA RATING:</b-th>
-              <b-th class="text-left">{{ebitdaRating}}</b-th>
+              <b-th class="text-left">{{ ebitdaRating }}</b-th>
             </b-tr>
             <b-tr>
               <b-th class="text-center">PRICE TO BOOK RATIO:</b-th>
-              <b-th class="text-left">{{priceToBookRatio}}</b-th>
+              <b-th class="text-left">{{ priceToBookRatio }}</b-th>
             </b-tr>
             <b-tr>
               <b-th class="text-center">PROFIT MARGIN:</b-th>
-              <b-th class="text-left">{{profitMargin}}</b-th>
+              <b-th class="text-left">{{ profitMargin }}</b-th>
             </b-tr>
           </b-tbody>
         </b-table-simple>
@@ -82,24 +82,22 @@
 <script>
 export default {
   name: "Analysis",
-  props:['financeData'],
-  data(){
-    return{
-      description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-      trendingBand:'BULLISH',
-      rsi: 'BULLISH',
-      macd:'BULLISH',
-      volume: 'BULLISH',
-      priceToEarnings:'GOOD',
-      ebitdaRating:'FAIR',
-      priceToBookRatio:'GOOD',
-      profitMargin:'LOW'
-
-    }
-  }
-}
+  props: { financeData: String, rsiData: String },
+  data() {
+    return {
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      trendingBand: "BULLISH",
+      rsi: "BULLISH",
+      macd: "BULLISH",
+      volume: "BULLISH",
+      priceToEarnings: "GOOD",
+      ebitdaRating: "FAIR",
+      priceToBookRatio: "GOOD",
+      profitMargin: "LOW",
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,35 +1,29 @@
 <template>
   <div>
-      <div v-if="stockSym">
-        {{updateStocks()}}
-        <vue-trading-view
-            :key="stockSym"
-            :options="{
-              symbol: stockSym,
-              theme: 'light',
-              interval: 'D',
-              width: 1000,
-              allow_symbol_change: true
-            }"
-        ></vue-trading-view>
-      </div>
+    <div v-if="stockSym">
+      {{ updateStocks() }}
+      <vue-trading-view
+        :key="stockSym"
+        :options="{
+          symbol: stockSym,
+          theme: 'light',
+          interval: 'D',
+          width: 1000,
+          allow_symbol_change: true,
+        }"
+      ></vue-trading-view>
+    </div>
 
     <div>
-      <analysis :financeData="financialData"></analysis>
-
-
+      <analysis :financeData="band" :rsiData="rsiData"></analysis>
     </div>
   </div>
 </template>
 
 <script>
-
-
-
-
 import VueTradingView from "vue-trading-view";
 import Analysis from "@/components/Analysis";
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "Chart",
@@ -38,44 +32,79 @@ export default {
     VueTradingView,
   },
 
-  data(){
-    return{
-      url:'',
-      stockPrice:this.$store.state.stockPrice,
-      financialData:{}
-    }
-
+  data() {
+    return {
+      url: "",
+      stockPrice: this.$store.state.stockPrice,
+      financialData: {},
+      bandData: {},
+      upperBand: 0,
+      lowerBand: 0,
+      band: "",
+      rsi: "",
+      rsiData: "",
+    };
   },
-  computed:{
-    stockSym(){
-      console.log('symbol', this.$store.state.symbol)
-      return this.$store.state.symbol
-    }
+  computed: {
+    stockSym() {
+      console.log("symbol", this.$store.state.symbol);
+      return this.$store.state.symbol;
+    },
   },
-  methods:{
-    async updateStocks(){
-
-      if(!this.$store.state.clearDataObj){
-        return
+  methods: {
+    async updateStocks() {
+      if (!this.$store.state.clearDataObj) {
+        return;
       }
       try {
-        await axios.get(
-            //  pk_b126cb1ba8dc441aa3673f3ccb2a3ba5
-            // sk_9fea5a3a643e4dc3b771899b3a642177
-            `https://cloud.iexapis.com/stable/stock/${this.$store.state.symbol}/quote?token=pk_b126cb1ba8dc441aa3673f3ccb2a3ba5`
-        )
-            .then((res) => {
-              this.financialData = res;
-              console.log("financialData", res.data);
+        await axios
+          .all([
+            axios.get(
+              `https://cloud.iexapis.com/stable/stock/${this.$store.state.symbol}/quote?token=sk_9fea5a3a643e4dc3b771899b3a642177`
+            ),
+            axios.get(
+              `https://cloud.iexapis.com/stable/stock/${this.$store.state.symbol}/indicator/bbands?range=1m&token=sk_9fea5a3a643e4dc3b771899b3a642177`
+            ),
+            axios.get(
+              `https://cloud.iexapis.com/stable/stock/${this.$store.state.symbol}/indicator/rsi?range=1m&token=sk_9fea5a3a643e4dc3b771899b3a642177`
+            ),
+          ])
+          .then(
+            axios.spread((res1, res2, res3) => {
+              this.financialData = res1;
               this.$store.state.stockPrice = this.financialData.data.latestPrice;
-            });
-      } catch (err){
-        console.log('error', err)
+              this.bandData = res2.data.indicator;
+              this.upperBand = this.bandData[2].slice(-1).pop();
+              this.lowerBand = this.bandData[0].slice(-1).pop();
+              this.rsi = res3.data.indicator[0].slice(-1).pop();
+              console.log("==>>", this.rsi);
+            })
+          );
+      } catch (err) {
+        console.log("error", err);
       }
-
       this.$store.state.clearDataObj = false;
+      if (this.$store.state.stockPrice > this.upperBand) {
+        this.$store.state.band = "BULLISH";
+      } else if (this.$store.state.stockPrice < this.lowerBand) {
+        this.$store.state.band = "BEARISH";
+      } else if (
+        this.upperBand > this.$store.state.stockPrice &&
+        this.$store.state.stockPrice > this.lowerBand
+      ) {
+        this.$store.state.band = "NEUTRAL";
+      }
+      this.band = this.$store.state.band;
+      //RSI if-then statements
+      if (this.rsi > 70) {
+        this.rsiData = "BEARISH";
+      } else if (this.rsi < 35) {
+        this.rsiData = "BULLISH";
+      } else if (35 < this.rsi && 70 > this.rsi) {
+        this.rsiData = "NEUTRAL";
+      }
       this.forceUpdate();
-    }
-  }
+    },
+  },
 };
 </script>
